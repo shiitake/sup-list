1a9766028c6b7ab28a7d9ce65b1d56b8331636bb
Shannon Barrett <shannon.barrett@digintent.com>
2016-05-02 17:31:48 -0500

    Commit all the things

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+﻿var express = require('express');
+var router = express.Router();
+var services = require('../data/services.json');
+
+
+/* GET home page. */
+router.get('/', function (req, res) {
+	res.render('index', { title: 'Usenet Provider Guide', services: services });
+});
+
+module.exports = router;
