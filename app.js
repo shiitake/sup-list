3d786d1cd49e2ce102973d263f01af945b881d3e
Shannon Barrett <sbarrett00@live.com>
2017-03-13 09:12:47 -0500

    Updated jade to pug

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('port', process.env.PORT || 3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
